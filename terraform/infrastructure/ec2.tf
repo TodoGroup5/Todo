11ee34baf940e6b2e02886c75f43620b212e68d3
Cindi-Ann Jeffery <cindi-anne.jeffery@bbd.co.za>
2025-06-04 18:05:23 +0200

    budgets

--- terraform/infrastructure/ec2.tf
+++ terraform/infrastructure/ec2.tf
@@ -1,6 +1,5 @@
-# EC2 Instance (placeholder AMI/instance type)
-resource "aws_instance" "web" {
-  ami                         = "ami-0c55b159cbfafe1f0" # Use a valid AMI for your region
+# EC2 Instance
+resource "aws_instance" "web" {                     
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
