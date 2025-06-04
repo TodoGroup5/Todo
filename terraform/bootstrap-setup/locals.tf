4b9f34ad3a7426e94e6db67ce49680267061355c
Cindi-Ann Jeffery <cindi-anne.jeffery@bbd.co.za>
2025-06-04 22:07:31 +0200

    session token

--- terraform/bootstrap-setup/locals.tf
+++ terraform/bootstrap-setup/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  actual_thresholds = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+  actual_thresholds = [20, 40, 60, 80, 90,]
   subscribers = ["cindi-anne.jeffery@bbd.co.za","Gregory.Conroy@bbd.co.za","joy@bbd.co.za","Dino.Gironi@bbd.co.za","rudolphe@bbdsoftware.com"]
 }
