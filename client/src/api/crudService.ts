d2c74f20fe80a6f4de92a31085d1d704fd26bd1f
digir <dino.gironi@bbd.co.za>
2025-06-11 00:33:20 +0200

    Update migrations in deployment

--- client/src/api/crudService.ts
+++ client/src/api/crudService.ts
@@ -20,7 +20,7 @@ export class CrudService {
 
 	//----- Request wrapper utils -----//
 	// S is the expected result.data.data type if successful
-	private static async makeRequest<S>(url: string, method: string, body?: any, options?: CrudOptions): Promise<ApiResponse<S>> {
+	private static async makeRequest<S>(url: string, method: string, body?: RequestInit["body"], options?: CrudOptions): Promise<ApiResponse<S>> {
 		try {
 			const headers = this.getHeaders(options?.headers);
 			const queryParams = options?.params
@@ -57,19 +57,19 @@ export class CrudService {
 
 	//----- CRUD functions -----//
 	// R is the expected result.data.data type if successful
-	static create<T, R>(endpoint: string, data: T, options?: CrudOptions): Promise<ApiResponse<R>> {
+	static create<T extends RequestInit["body"], R>(endpoint: string, data: T, options?: CrudOptions): Promise<ApiResponse<R>> {
 		return this.makeRequest<R>(endpoint, "POST", data, options);
 	}
 	static read<R>(endpoint: string, options?: CrudOptions): Promise<ApiResponse<R>> {
 		return this.makeRequest<R>(endpoint, "GET", undefined, options);
 	}
-	static update<T, R>(endpoint: string, id: string | number, data: Partial<T>, options?: CrudOptions): Promise<ApiResponse<R>> {
+	static update<T extends RequestInit["body"], R>(endpoint: string, id: string | number, data: T, options?: CrudOptions): Promise<ApiResponse<R>> {
 		return this.makeRequest<R>(`${endpoint}/${id}`, "PUT", data, options);
 	}
 	static delete<R>(endpoint: string, id: string | number, options?: CrudOptions): Promise<ApiResponse<R>> {
 		return this.makeRequest<R>(`${endpoint}/${id}`, "DELETE", undefined, options);
 	}
-	static customRequest<R>(endpoint: string, method: string, body?: any, options?: CrudOptions): Promise<ApiResponse<R>> {
+	static customRequest<R>(endpoint: string, method: string, body?: RequestInit["body"], options?: CrudOptions): Promise<ApiResponse<R>> {
 		return this.makeRequest<R>(endpoint, method, body, options);
 	}
 
